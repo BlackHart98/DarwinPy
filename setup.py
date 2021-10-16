fe4bc532d1efb1da855bff818966a880a63ee1c4
Pius Arhanbhunde <15337143+BlackHart98@users.noreply.github.com>
2021-10-16 12:08:33 +0100

    update repository

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+"Development Status :: 5 - Production/stable",
+"Intended Audience :: Developers",
+"Operating System :: Microsoft :: Windows :: Windows 10",
+"License :: OSI Approved :: MIT License",
+"Programming Language :: Python :: 3"
+]
+
+
+setup(
+name = "DarwinPy",
+version = "0.0.1",
+description = "A evolutionary computation module",
+long_description = open("README.md").read() + "\n\n" + open("CHANGELOG.txt").read(),
+url = "",
+author = "Pius Arhanbhunde",
+author_email = "pjacks419@gmail.com",
+license = "MIT License",
+classifiers = classifiers,
+keywords = "evolution",
+packages = find_packages(),
+install_requires = ['numpy']
+)
--- test.py
+++ test.py
@@ -13,66 +13,15 @@ def hammingDist(goal, matrix):
 
 
 if __name__ == "__main__":
-    processes = 3
     mouse_population = 5
     mutation_rate = 0.5
     search_space = (0,1)
     goal = np.array([1,0,1,1,0,1,1, 0,1,1,0],int)
     chromosome_length = len(goal)
 
-    # mouse_species = (
-    # DarwinPy.DarwinPyCore(processes, "Genetics"))
-    #
-    # mouse_species.initializeGenetics(chromosome_length,
-    # mouse_population,search_space,int)
-    #
-    # print(mouse_species)
-    #
-    # print(mouse_species.getChromosomeMatrixList())
-    # chromosome_matrix_list = mouse_species.getChromosomeMatrixList()
-    # fitness_matrix = np.array([[0]*mouse_population]*processes,float)
-    #
-    # for i in range(processes):
-    #     # print("Oh!")
-    #     # print(chromosome_matrix_list[i])
-    #     fitness_matrix[i] = hammingDist(goal, chromosome_matrix_list[i])
-    #
-    # print(fitness_matrix)
-    #
-    # is_goal = False
-    # gen = 1
-    # while not is_goal:
-    #     print("Generation #{}\n".format(gen))
-    #     gen += 1
-    #     mouse_species.evolve(fitness_matrix,
-    #     mutation_rate, 0.5)
-    #     print("get mouse matrix:\n {}".
-    #     format(mouse_species.getChromosomeMatrixList()))
-    #     for i in range(processes):
-    #         fitness_matrix[i] = hammingDist(goal,
-    #         chromosome_matrix_list[i])
-    #     print("get fitness matrix: {}".format(fitness_matrix))
-    #     i = 0
-    #     # is_goal = True
-    #     while (not is_goal) and (i < processes):
-    #         if chromosome_length in fitness_matrix[i]:
-    #             is_goal = True
-    #         i += 1
-
-
-    # mouse_species_async.populateInstancesGenetics()
-
-    # print(mouse_species_async.getGenerationMatrices())
-
-    # chromosome_length = 8
-    # mouse_population = 5
-    # mutation_rate = 0.05
-    #
     mouse_species = DarwinPy.Genetics.Genetics(chromosome_length,
     mouse_population, (0,1), int)
     print("mouse species instantiated:\n {}".format(mouse_species))
-
-    # goal = np.array([1,0,1,1,0,1,1, 0],int)
 
     print("the goal:\n {}".format(goal))
 
@@ -103,43 +52,3 @@ if __name__ == "__main__":
         print("get fitness vector: {}".format(fitness_vector))
         if chromosome_length in fitness_vector:
             is_goal = True
-
-
-    # mouse_parent = 5
-    # mouse_children = 3
-    # mouse_species = DarwinPy.EvolutionaryStrategy.EvolutionaryStrategy(chromosome_length,
-    # mouse_parent, mouse_children, (0,1), int)
-    # print(mouse_species)
-    # print("\n")
-    # mouse_species.generateParent()
-    # print("get (parent) mouse matrix(ES):\n {}".
-    # format(mouse_species.getParentGeneration()))
-    #
-    # gen = 1
-    # is_goal = False
-    # while not is_goal:
-    #     print("Generation #{}\n".format(gen))
-    #     fitness_vector_1 = np.array(
-    #     hammingDist(goal,mouse_species.getParentGeneration()),
-    #     float)
-    #
-    #     print("get parent fitness(ES): {}".
-    #     format(fitness_vector_1))
-    #
-    #     mouse_species.generateOffspring(mutation_rate, fitness_vector_1)
-    #     print("get (children) mouse matrix(ES):\n {}".
-    #     format(mouse_species.getChildrenGeneration()))
-    #
-    #     fitness_vector_2 = np.array(
-    #     hammingDist(goal,mouse_species.getChildrenGeneration()),
-    #     float)
-    #
-    #     print("get children fitness(ES): {}".
-    #     format(fitness_vector_2))
-    #
-    #     mouse_species.selectNextGeneration(fitness_vector_1,fitness_vector_2)
-    #     print("get (parent) mouse matrix(ES):\n {}".
-    #     format(mouse_species.getParentGeneration()))
-    #     gen += 1
-    #     if chromosome_length in fitness_vector_1:
-    #         is_goal = True
